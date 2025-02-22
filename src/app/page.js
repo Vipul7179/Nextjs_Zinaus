20644370fb90a2a6ff9cdec428c7307cfb9dceeb
Vipul7179 <vpsvipulsingh100@gmail.com>
2025-02-22 15:04:17 +0530

    some changes

--- src/app/page.js
+++ src/app/page.js
@@ -86,7 +86,7 @@ export default function Home() {
 
       <div className="ourservices-button">
       <div className="ourservices-textbutton">
-    <a>All Services</a>
+      <a class="text-white">All Services</a>
   </div>
       </div>
 
@@ -97,12 +97,12 @@ export default function Home() {
     {/* <!-- Service Box 1 --> */}
     <div class="service-box">
       <div>
-      <img src="/Images/online-store.png" alt="Marketplace Icon" class="service-icon" />
+      <img src="/Images/online-store.png" alt="ecommerce Icon" class="service-icon" style={{paddingRight: "3px"}} />
       </div>
       <div>
       <h3 class="service-title">E-commerce</h3>
       <p class="service-description">
-      We offer a seamless e-commerce platform with easy product navigation, secure payments, and fast delivery to help businesses thrive online.
+      We offer a seamless e-commerce platform with intuitive navigation, secure payments, fast delivery, and a smooth checkout, along with mobile optimization and order tracking.
       </p>
       </div>
     </div>
@@ -110,7 +110,7 @@ export default function Home() {
     {/* <!-- Service Box 2 --> */}
     <div class="service-box">
       <div>
-      <img src="/Images/ui-design.png" alt="Ecommerce Icon" class="service-icon" />
+      <img src="/Images/ui-design.png" alt="UI Icon" class="service-icon" />
       </div>
       <div>
       <h3 class="service-title">Mobile App Development (iOS & Android)</h3>
@@ -123,12 +123,12 @@ export default function Home() {
     {/* <!-- Service Box 3 --> */}
     <div class="service-box">
       <div>
-      <img src="/Images/cards.png" alt="Marketing Icon" class="service-icon" />
+      <img src="/Images/cards.png" alt="cards Icon" class="service-icon" style={{paddingRight: "3px"}} />
       </div>
       <div>
       <h3 class="service-title">Social Casino Gaming Platform</h3>
       <p class="service-description">
-      We develop engaging social casino games that offer players a fun, interactive experience. Our custom-built solutions provide exciting gameplay, smooth performance, and social features, ensuring your game stands out in the competitive gaming market.
+      We develop engaging social casino games with fun, interactive experiences. Our custom solutions ensure exciting gameplay, smooth performance, and social features to stand out.
       </p>
       </div>
     </div>
@@ -136,12 +136,12 @@ export default function Home() {
     {/* <!-- Service Box 4 --> */}
     <div class="service-box">
       <div>
-      <img src="/Images/movie.png" alt="SaaS Icon" class="service-icon" />
+      <img src="/Images/movie.png" alt="movie Icon" class="service-icon" />
       </div>
       <div>
       <h3 class="service-title">Photography & Video Content for Promotions</h3>
       <p class="service-description">
-      We offer professional photography and video content to enhance your promotional campaigns. From product shoots to captivating video ads, we create high-quality visuals that grab attention and boost your brand’s engagement.
+      We provide professional photography and video content for promotions, creating high-quality product shoots and captivating ads that enhance engagement and boost your brand.
       </p>
       </div>
     </div>
@@ -149,12 +149,12 @@ export default function Home() {
     {/* <!-- Service Box 5 --> */}
     <div class="service-box">
       <div>
-      <img src="/Images/testing.png" alt="Cloud Icon" class="service-icon" />
+      <img src="/Images/testing.png" alt="qa Icon" class="service-icon" />
       </div>
       <div>
       <h3 class="service-title">Quality Assurance & Testing Solutions</h3>
       <p class="service-description">
-      We provide comprehensive QA testing, including manual testing, automation, and API performance testing, ensuring your products are bug-free, reliable, and optimized for peak performance across all platforms.
+      We offer comprehensive QA testing, including manual, automation, and API performance testing, ensuring bug-free, reliable, and high-performance products across all platforms.
       </p>
       </div>
     </div>
@@ -162,18 +162,22 @@ export default function Home() {
     {/* <!-- Service Box 6 --> */}
     <div class="service-box">
       <div>
-      <img src="/Images/app-development.png" alt="Mobile Icon" class="service-icon" />
+      <img src="/Images/app-development.png" alt="aoo dev Icon" class="service-icon" style={{paddingRight:"3px"}}/>
       </div>
       <div>
       <h3 class="service-title">Web Development</h3>
       <p class="service-description">
-      We specialize in developing custom web applications that provide innovative and scalable solutions. Our experienced team creates high-performance applications tailored to your business, enhancing user experiences and driving growth.
+      We develop custom web applications with innovative, scalable solutions. Our expert team builds high-performance apps tailored to your business, enhancing user experience and driving growth.
       </p>
       </div>
     </div>
   </div>
 </section>
 {/* our services section end */}
+
+<section className="helpsection">
+
+</section>
       
       </main>
       
