ea58115c643cf14750b15fc9f05d7d1f896bb11a
Vipul7179 <vpsvipulsingh100@gmail.com>
2025-03-22 16:15:19 +0530

    updated footer icons and about section

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="footer">
@@ -19,10 +22,10 @@ function Footer() {
         </div>
 
         <div className="footer-social">
-          <a href="#" className="social-icon">Facebook</a>
-          <a href="#" className="social-icon">LinkedIn</a>
-          <a href="#" className="social-icon">Twitter</a>
-          <a href="#" className="social-icon">Instagram</a>
+        <a href="#" className="social-icon"><FaFacebookSquare  size={38} /></a>
+          <a href="#" className="social-icon"><FaLinkedin size={38} /></a>
+          <a href="#" className="social-icon"><FaSquareXTwitter size={38} /></a>
+          <a target='blank' href="https://www.instagram.com/sculpnova/#" className="social-icon"><FaSquareInstagram  size={39}  /></a>
         </div>
       </div>
 
